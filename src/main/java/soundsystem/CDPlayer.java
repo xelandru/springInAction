c2ae1d9dfe50566d860362842afcf0879f188495
adascalu <alexandru.dascalu@adswizz.com>
2019-01-04 16:03:49 +0200

    Chapter 2 AutoWiring

---
+++ src/main/java/soundsystem/CDPlayer.java
@@ -0,0 +1,28 @@
+package soundsystem;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CDPlayer implements MediaPlayer {
+
+    private CompactDisc cd;
+
+
+    @Autowired
+    public CDPlayer(CompactDisc cd) {
+        this.cd = cd;
+    }
+
+    @Autowired
+
+    public void setCd(CompactDisc cd) {
+        this.cd = cd;
+    }
+
+
+    @Override
+    public void play() {
+        cd.play();
+    }
+}
