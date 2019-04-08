d9f26e9b3dc3b19a355934f0932a4480af68aa27
adascalu <alexandru.dascalu@adswizz.com>
2019-04-08 16:49:27 +0300

    Chapter 5

--- src/main/java/chapter5/spittr/config/SpittrWebAppInitializer.java
+++ src/main/java/chapter5/spittr/config/SpittrWebAppInitializer.java
@@ -1,4 +1,22 @@
 package chapter5.spittr.config;
 
-public class SpittrWebAppInitializer {
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{RootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
--- src/main/java/chapter5/spittr/config/WebConfig.java
+++ src/main/java/chapter5/spittr/config/WebConfig.java
@@ -1,4 +1,11 @@
 package chapter5.spittr.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("spittr.web")
 public class WebConfig {
 }
